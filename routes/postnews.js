79a8844b768ba2a43a8ab2cb81b27f7b6de24ff3
Laurent Lepinay <sigomatix@hotmail.com>
2012-07-21 01:11:21 +0200

    ajout vue pour toutes les news et route

--- routes/postnews.js
+++ routes/postnews.js
@@ -1,10 +1,17 @@
 module.exports = function(app) {
 
+	var news = ["Some first news", "Some second <b>news</b> !"];
+
+	app.get('/allnews', function(req, res) {
+        res.render("allnews", {news:news});
+    });
+
     app.get('/postnews', function(req, res) {
         res.render("postnews");
     });
 
     app.post('/postnews', function(req, res, next) {
-        res.end(req.body.post);
+        news.push(req.body.post);
+        res.redirect('/allnews');
     });
 };
