b5f87358eab2b7457c8f7901ed2c97c42b0698d8
Laurent Lepinay <sigomatix@hotmail.com>
2012-07-21 01:54:22 +0200

    Ajout gestion du titre et date dans les post de news

--- routes/postnews.js
+++ routes/postnews.js
@@ -1,6 +1,16 @@
 module.exports = function(app) {
 
-	var news = ["Some first news", "Some second <b>news</b> !"];
+	var news = [
+	{
+		title:"First title",
+		post:"Some first news",
+		date:new Date()
+	}, 
+	{
+		title:"Second title",
+		post:"Some second <b>news</b> !",
+		date:new Date()
+	}];
 
 	app.get('/allnews', function(req, res) {
         res.render("allnews", {news:news});
@@ -11,7 +21,11 @@ module.exports = function(app) {
     });
 
     app.post('/postnews', function(req, res, next) {
-        news.push(req.body.post);
+        news.push({
+        	title:req.body.title,
+			post:req.body.post,
+			date:new Date()
+		});
         res.redirect('/allnews');
     });
 };
